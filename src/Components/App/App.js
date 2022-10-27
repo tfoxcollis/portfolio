868c333c27818c304cfd1ca418d6fd653d083822
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-10-26 18:59:49 -0700

    initial set up

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './App.css';
+import Nav from '../Nav/Nav';
+
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App

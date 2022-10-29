ef8fe4e25388bce01aa87d10b8a2f617bf2fec02
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-10-28 23:02:24 -0700

    add project images

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Projects.css'
+import boafGroup from '../../assets/boaf-group.png'
+import boafSolo from '../../assets/boaf-solo.png'
+import screamStreams from '../../assets/scream-streams.png'
+import zen from '../../assets/zen.png'
 
 const Projects = () => {
   return (
@@ -10,27 +14,38 @@ const Projects = () => {
       <div className="box-container">
         <div className="box">
           <div className="imgbox">
-            <img src="https://i.postimg.cc/7PVqmm2w/1581873309154.jpg" />
+            <img src={boafGroup}/>
           </div>
           <div className="content">
-            <h2> Carlton Agesa<br /> creative developer</h2>
+            <h2> Birds of a Feather<br />Group- React with Graphql</h2>
           </div>
         </div>
         <div className="box">
           <div className="imgbox">
-            <img src="https://i.postimg.cc/Rh8sXk1g/2004649.jpg" />
+            <img src={boafSolo}/>
           </div>
           <div className="content">
-          <h2>Twitter<br />@carl_agesa</h2>
+          <h2>Birds of a Feather<br />Solo- React</h2>
           </div>
         </div>
         <div className="box">
           <div className="imgbox">
-            <img src="https://i.postimg.cc/7PVqmm2w/1581873309154.jpg" />
+            <img src={screamStreams} />
+            </div>
+          <div className="content">
+          <h2>Scream Streams<br />Paired- React</h2>
+          </div>
+        </div>
+        <div className="box">
+          <div className="imgbox">
+            <img src={zen} />
+            </div>
+          <div className="content">
+          <h2>Zen in Ten<br />Group- React with Typescript</h2>
           </div>
         </div>
       </div>
-    </div>
+      </div>
   )
 }
 

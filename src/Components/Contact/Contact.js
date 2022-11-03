f821aedb135e52b6409a80e54c4702b30d189192
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-11-02 23:54:41 -0700

    Implement Mailer

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFighterJet, faEnvelope, faUser, faAt, faComment, faCircleXmark, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ot7m8ye', 'template_uxsodin', form.current, 'rA8GGTB7ZpaSgBPLT')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div id="contactContainer" className="form-container">
       <h2 className="front-text text-white">Contact Me</h2>
@@ -13,7 +28,7 @@ const Contact = () => {
       <input type="checkbox" id="frmContactForm" />
       <div className="contact-modal">
         <div className="contact-form">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="contact-wrapper">
               <div className="contact-section">
                 <h3 className="p-none m-none mb-quarter text-white">
@@ -24,18 +39,19 @@ const Contact = () => {
                 </h3>
               </div>
               <div className="contact-section">
+                <input type="hidden" name="toName" value="Trish"></input>
                 <div className="form-item">
-                  <input type="text" id="txtFullName" placeholder="name" />
+                  <input type="text" id="txtFullName" name="txtFullName" placeholder="name" />
                   <label className="lbl-floating" for="txtFullName">Name</label>
                   <FontAwesomeIcon className="icon text-white" icon={ faUser } />
                 </div>
                 <div className="form-item">
-                  <input type="text" id="txtEmail" placeholder="email" />
+                  <input type="text" id="txtEmail" name="txtEmail" placeholder="email" />
                   <label className="lbl-floating" for="txtEmail">Email</label>
                   <FontAwesomeIcon className="icon text-white" icon={ faAt } />
                 </div>
                 <div className="form-item">
-                  <textarea id="txtContent" placeholder="This is where you tell me about your job opportunity :)"></textarea>
+                  <textarea id="txtContent" name="txtContent" placeholder="This is where you tell me about your job opportunity :)"></textarea>
                   <label className="lbl-floating" for="txtContent">{"This is where you tell me about your job opportunity :)"}</label>
                   <FontAwesomeIcon className="icon text-white" icon={ faComment } />
                 </div>
@@ -45,7 +61,7 @@ const Contact = () => {
                   <FontAwesomeIcon icon={faCircleXmark} />
                   <span className="ml-quarter">Cancel</span>
                 </label>
-                <button className="btn-open ml-whole" type="button">
+                <button className="btn-open ml-whole" type="submit">
                   <FontAwesomeIcon icon={faPaperPlane} />
                   <span className="ml-half"><label for="frmContactForm">Send</label></span>
                 </button>

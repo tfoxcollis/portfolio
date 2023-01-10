94c922c96f0b63e0a2c24426037cf122ef48a49b
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-01-10 10:22:06 -0800

    revert back to css

--- src/Components/TitleSection/TitleSection.js
+++ src/Components/TitleSection/TitleSection.js
@@ -10,7 +10,7 @@ const TitleSection = () => {
     <div className='title-container'>
       <video src={video5} autoPlay loop muted />
       <h1>Frontend Software Developer</h1>
-      <p>What are you waiting for?</p>
+      <p>&#10023; React &#10023; Javascript &#10023; CSS &#10023; HTML &#10023; </p>
       <Contact />
     </div>
   )
